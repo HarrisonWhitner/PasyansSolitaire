ce0cb02fb651ac8ab7ff1a1b9220dcfea683b8ea
HarrisonWhitner <hwhit16@vt.edu>
2020-08-28 22:37:58 -0400

    Add python package install to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Pasyans install script - Harrison Whitner - 08/28/20
 
+pip3 install termcolor                                   # install a necessary package for colored text in the shell
+
 sudo cp pasyans.py -t /usr/local/bin/                    # copy python script to install dir
 sudo chmod 755 /usr/local/bin/pasyans.py                 # change the perms for the script
 sudo mv /usr/local/bin/pasyans.py /usr/local/bin/pasyans # rename the script to remove the extension
